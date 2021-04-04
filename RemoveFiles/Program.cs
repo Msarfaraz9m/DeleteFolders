5a15d2c4fc39756c3c0fc5b7655bae3fcf2eb5ab
Mohd Sarfaraz <sarfaraz.mohd2424@gmail.com>
2021-04-05 00:00:45 +0530

    Project to delete bin,obj

---
+++ RemoveFiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RemoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoveUnwantedFileFromLocation(@"X:\");
+        }
+
+        private static void RemoveUnwantedFileFromLocation(string directory)
+        {
+            List<string> filesToRemove = new List<string> { "bin", "obj" };
+            foreach (var item in Directory.EnumerateDirectories(directory))
+            {
+                if (filesToRemove.Any(y => item.Substring(item.LastIndexOf('\\') + 1) == y))
+                    Directory.Delete(item, true);
+                else
+                    RemoveUnwantedFileFromLocation(item);
+            }
+        }
+    }
+}
